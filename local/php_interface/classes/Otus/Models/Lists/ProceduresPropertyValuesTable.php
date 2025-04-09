0ad68fde8137d7be5b469f4f9d19ca062a933c93
max1m-0 <esidoz24@mail.ru>
2025-04-09 12:38:38 +0300

    added healers and procedures ORM

---
+++ local/php_interface/classes/Otus/Models/Lists/ProceduresPropertyValuesTable.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Otus\Models\Lists;
+
+use Bitrix\Main\Entity\ReferenceField;
+use Otus\Models\AbstractIblockPropertyValuesTable;
+
+class ProceduresPropertyValuesTable extends AbstractIblockPropertyValuesTable
+{
+    public const IBLOCK_ID = 17;
+}
